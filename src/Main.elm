8e14910664bcd75661d71742d360aa137266db8e
Joshie Fishbein <joshie@sanctuary.computer>
2019-11-15 13:21:20 -0500

    add request and dom

--- src/Main.elm
+++ src/Main.elm
@@ -1,69 +1,123 @@
-module Main exposing(main)
+module Main exposing (main)
 
 import Browser
-import Browser.Navigation as Nav
 import Html exposing (..)
 import Html.Attributes exposing (..)
-import Url
+import Html.Events exposing (..)
+import Http
+import Json.Decode exposing (Decoder, field, string)
 
-main : Program () Model Msg
+
+
+-- MAIN
+
+
 main =
-  Browser.application
+  Browser.element
     { init = init
-    , view = view
     , update = update
     , subscriptions = subscriptions
-    , onUrlChange = UrlChanged
-    , onUrlRequest = LinkClicked
+    , view = view
     }
 
-type alias Model =
-  { key : Nav.Key
-  , url : Url.Url
-  }
 
 
-init : () -> Url.Url -> Nav.Key -> ( Model, Cmd Msg )
-init flags url key =
-  ( Model key url, Cmd.none )
+-- MODEL
+
+
+type Model
+  = Failure
+  | Loading
+  | Success String
+
+
+init : () -> (Model, Cmd Msg)
+init _ =
+  (Loading, getRandomCatGif)
+
+
+
+-- UPDATE
+
 
 type Msg
-  = LinkClicked Browser.UrlRequest
-  | UrlChanged Url.Url
+  = MorePlease
+  | GotGif (Result Http.Error String)
 
 
-update : Msg -> Model -> ( Model, Cmd Msg )
+update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
-    LinkClicked urlRequest ->
-      case urlRequest of
-        Browser.Internal url ->
-          ( model, Nav.pushUrl model.key (Url.toString url) )
+    MorePlease ->
+      (Loading, getRandomCatGif)
 
-        Browser.External href ->
-          ( model, Nav.load href )
+    GotGif result ->
+      case result of
+        Ok url ->
+          (Success url, Cmd.none)
 
-    UrlChanged url ->
-      ( { model | url = url }
-      , Cmd.none
-      )
+        Err _ ->
+          (Failure, Cmd.none)
+
+
+
+-- SUBSCRIPTIONS
+
 
 subscriptions : Model -> Sub Msg
-subscriptions _ =
+subscriptions model =
   Sub.none
 
-view : Model -> Browser.Document Msg
-view model =
-  { title = "URL Interceptor"
-  , body =
-      [ h1 [] [ text "Get Elmed, Dog" ]
-      , ul []
-          [ viewLink "/"
-          , viewLink "/404"
-          ]
-      ]
-  }
 
-viewLink : String -> Html msg
-viewLink path =
-  li [] [ a [ href path ] [ text path ] ]
+
+-- VIEW
+
+view : Model -> Html Msg
+view model =
+  div [ class "p1" ]
+    [ viewGif model
+    ]
+
+
+viewGif : Model -> Html Msg
+viewGif model =
+  case model of
+    Failure ->
+      div []
+        [ text "I could not load a random mayo for some reason. "
+        , button [ onClick MorePlease ] [ text "Try Again!" ]
+        ]
+
+    Loading ->
+      text "Loading..."
+
+    Success url ->
+      div
+        [ class "p4 col-12 flex items-center justify-center"
+        , style "font-family" "helvetica"
+        ]
+        [ div [ class "col-6 mxauto" ]
+          [ div [ class "flex justify-center items-end mb3"] 
+            [ h1 [ class "m0 p0", style "font-size" "48px", style "height" "46px" ] [ text "MOAR CLOWNZ" ]
+            , button [ onClick MorePlease, class "block ml2 px1 py_25", style "border" "1px solid blue", style "border-radius" "3px" ] [ text "MOAR PLZ!" ]
+            ]
+          , img [ src url, class "col-12" ] []
+          ]
+        ]
+
+
+
+-- HTTP
+
+
+getRandomCatGif : Cmd Msg
+getRandomCatGif =
+  Http.get
+    { url = "https://api.giphy.com/v1/gifs/random?api_key=5DYyxYScARZshsn38FrpiLbFGVFLHcrb&tag=clown"
+    , expect = Http.expectJson GotGif gifDecoder
+    }
+
+
+gifDecoder : Decoder String
+gifDecoder =
+  field "data" (field "image_url" string)
